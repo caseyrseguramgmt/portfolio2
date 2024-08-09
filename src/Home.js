b301e8494876298c668999d0b67b0fbc46ae6602
Casey Segura <caseyrseguramgmt@gmail.com>
2024-08-09 12:40:05 -0400

    cicd check

--- src/Home.js
+++ src/Home.js
@@ -5,8 +5,8 @@ function Home() {
     <div className="text-center py-8">
       <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
       <p className="mt-4 text-lg">
-        <a href="http://localhost:5173/" target="_blank" rel="noopener noreferrer">
-          http://localhost:5173/
+        <a href="http://localhost:4173/" target="_blank" rel="noopener noreferrer">
+          http://localhost:4173/
         </a>
       </p>
     </div>
